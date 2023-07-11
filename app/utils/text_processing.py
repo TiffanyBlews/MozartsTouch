00dedd5382d4490e507449834745e27a41236147
WangTN <31386322+WangTooNaive@users.noreply.github.com>
2023-07-12 00:24:02 +0800

    Update text_processing.py

--- app/utils/text_processing.py
+++ app/utils/text_processing.py
@@ -2,7 +2,7 @@ from pathlib import Path
 app_path = Path(__file__).resolve().parent.parent
 
 async def test(txt):
-    return path/"static"/"BONK.mp3"
+    return app_path/"static"/"BONK.mp3"
 
 async def mubert(txt):
     pass
