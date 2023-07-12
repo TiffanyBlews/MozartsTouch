b6114cf0ec643994c7f14f393b27177bd5b1ef37
WangTN <wangtheneighbor@gmail.com>
2023-07-12 21:59:59 +0800

    Edit music_generation.py

--- app/utils/music_generation.py
+++ app/utils/music_generation.py
@@ -4,6 +4,7 @@ import base64
 app_path = Path(__file__).resolve().parent.parent
 
 def test_music_gen(txt: str) -> Bytes:
+    print("音乐生成提示词：" + txt)
     test_path = app_path/"static"/"BONK.mp3"
     test_mp3 = open(test_path,"rb").read()
     return base64.b64encode(test_mp3)
