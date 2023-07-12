f5a220d715ff671159adfad3f12ec4010fca691c
WangTN <wangtheneighbor@gmail.com>
2023-07-12 19:53:46 +0800

    Add annotation

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
-from typing import BinaryIO
 from fastapi import FastAPI, File, UploadFile, Form
-# from fastapi.responses import HTMLResponse
 # from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
@@ -17,11 +15,19 @@ from utils.text_processing import test, mubert, riffusion
 ci = Interrogator(ci_config)
 
 app_path = Path(__file__).parent
-app = FastAPI()
+
+app = FastAPI(title='点彩成乐',description='“点彩成乐”大创后端')
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 async def Diancai(img: Image, mode: int):
+    # 图片转文字
     txt = img2txt(ci, img)
-
+    # 文字生成音乐
     mode_dict={
         0: test,
         1: mubert,
@@ -41,14 +47,32 @@ async def get_bytes_from_url(url: str):
             return await response.read()
 
 @app.post("/upload")
-async def create_upload_file(file: UploadFile = File(...), mode: int = Form(...)):
-    img = Image.open(file.file)
+async def upload_file(file: UploadFile = File(...), mode: int = Form(...)):
+    '''
+    上传图片以进行音乐生成
+    Parameters:
+        - file: 图片文件，Content-Type: image/*
+        - mode: 指定生成模型（0:测试用；1:Mubert模型；2:Riffusion模型）
+    Return: 
+        - prompt: 图片转文字结果
+        - result: 生成的音频文件base64后的结果
+    '''
+    img = read_image_from_binary(file.file)
     return await Diancai(img, mode)
 
 @app.post("/upload-url")
 async def upload_url(*, url: str = Form(...), mode: int = Form(...)):
+    '''
+    上传图片链接以进行音乐生成
+    Parameters:
+        - url: 图片链接
+        - mode: 指定生成模型（0:测试用；1:Mubert模型；2:Riffusion模型）
+    Return: 
+        - prompt: 图片转文字结果
+        - result: 生成的音频文件base64后的结果
+    '''
     bytes = await get_bytes_from_url(url)
-    img = Image.open(BytesIO(bytes))
+    img = read_image_from_binary(BytesIO(bytes))
     return await Diancai(img, mode)
 
 @app.get("/")
